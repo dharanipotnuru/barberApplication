074d8edf1ee202e6d1e15ac3a9cc881018d2c0b3
potnurudharani <dharanipotnuru41@gmail.com>
2022-05-12 09:21:06 +0530

    First Commit

--- src/main/java/com/cappack13/controller/BarberControllerByNew.java
+++ src/main/java/com/cappack13/controller/BarberControllerByNew.java
@@ -2,16 +2,18 @@ package com.cappack13.controller;
 
 import javax.validation.Valid;
 
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cappack13.dao.BarberDaoForCustomer;
 import com.cappack13.model.Users;
 
-@Controller
+@RestController
 public class BarberControllerByNew {
 	
 	@Autowired
@@ -19,7 +21,7 @@ public class BarberControllerByNew {
 	
 	
 	Logger log=org.slf4j.LoggerFactory.getLogger(BarberControllerByAdmin.class);
-	@PostMapping(path="/createCustomer")
+	@PostMapping(path="/createCustomerbyAdmin")
 	public Users addCustomer(@RequestBody Users user) {
 		Users u = barDaoCus.addCustomer(user);
 		if(u!=null)
